b9177f586b4e2d2985b1956528137a225821e4c7
Luis Fernando Segalla <36170084+LouisFSegalla@users.noreply.github.com>
2020-02-19 09:03:53 -0300

    Add files via upload
    Modifiquei um pouco os scripts de pós processamento para melhor poder comparar as relações de erro entre o resultado numérico e analítico.

--- plotThogheter.py
+++ plotThogheter.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 #arquivos a serem usados
-name1 = "direções grosseira.txt"
-name2 = "direções Pós-Processamento.txt"
+name1 = "results/direções grosseira.txt"
+name2 = "results/direções Pós-Processamento.txt"
 
 
 #abre os arquivos na forma de uma lista
@@ -23,6 +23,7 @@ rho = np.zeros([tamX,rd*tamY])
 ux  = np.zeros([tamX,rd*tamY])
 uy  = np.zeros([tamX,rd*tamY])
 
+ux_fine = np.zeros([tamY])
 #-----------------------------------------------------------------CALCULANDO A DENSIDADE-----------------------------------------------------#
 rhoMed = 0
 it = 0
@@ -54,6 +55,8 @@ for i in range(0,tamX,1):
     for j in range(0,tamY,1):
         ux[i][j] = dataF[it][1] + dataF[it][5] + dataF[it][8] - (dataF[it][3] + dataF[it][6] + dataF[it][7])
         ux[i][j] = ux[i][j]/(rho[i][j] + 0.0000001)
+        if(i == 900):
+            ux_fine[j] = ux[i][j]
         it = it+1
 
 #--------------------------------------------------------------CALCULANDO A VELOCIDADE EM Y-----------------------------------------------------#
@@ -73,6 +76,30 @@ for i in range(0,tamX,1):
 
 
 
+###############################################################################
+raio = 19.5
+
+dy = 1.0
+dx = 1.0
+dt = 1.0
+
+tau = 0.56000000
+
+ni = (2*tau - 1)*(dx*dx) / (6*dt)
+
+v_max = 0.003
+
+dP = v_max * 4 * ni * dx / (raio*raio)
+
+vec = np.zeros([(rd*20)])
+
+print('ni: ',ni)
+print('dP: ', dP)
+
+for i in range(0,len(vec),1):
+    vec[i] = (dP*(raio*raio - np.abs((raio - i)*(raio - i)))) / (4*ni*dx)
+###############################################################################
+
 print(rhoMed / (tamX*(rd*tamY)))
 
 fig = plt.figure()
@@ -82,12 +109,16 @@ plt.ylabel('y')
 plt.xlabel('x')
 plt.title('Densidade no domínio')
 
+###############################################################################
+
 fig2 = plt.figure()
 cbUx = plt.imshow(ux)
 plt.colorbar(cbUx)
 plt.ylabel('y')
 plt.xlabel('x')
 plt.title('Velocidade na direção X no domínio')
+
+###############################################################################
 
 fig3 = plt.figure()
 cbUy = plt.imshow(uy)
@@ -96,11 +127,37 @@ plt.ylabel('y')
 plt.xlabel('x')
 plt.title('Velocidade na direção Y no domínio')
 
+###############################################################################
+
 fig4 = plt.figure()
-plt.plot(ux[900][:],'-')
+
+namelist = ['Analítico','Numérico']
+
+plt.plot(vec,'-')
+#plt.plot(ux_fine,'*',c='red')
+plt.plot(ux[900][:],'o')
+plt.legend(namelist,loc="upper right")
 plt.ylabel('y')
 plt.xlabel('x')
 plt.title('Perfil de velocidade em X')
 
+plt.show()
+###############################################################################
+fig5 = plt.figure()
+
+erro = np.zeros(len(vec))
+x    = np.arange(len(vec))
+
+#não sei se está certinho assim
+for i in range(len(vec)):
+    print(i, ' | analitoco: ', vec[i], ' | numérico: ', ux[900][i])
+    #erro[i] = (np.absolute(vec[i] - ux[900][i]) / (vec[i] + 0.0001))*100
+    erro[i] = np.absolute(vec[i] - ux[900][i])
+
+plt.bar(x, erro, align='center', alpha=0.5)
+plt.xticks(x, x)
+plt.ylabel('Diferença')
+plt.xlabel('Posição')
+plt.title('Erro em diferença absoluta')
 
 plt.show()
--- plotVelocity.py
+++ plotVelocity.py
@@ -18,7 +18,59 @@ namelist = ['Refinado','Grosseiro']
 
 plt.plot(vec1,c='red')
 plt.plot(vec2,c='blue')
-plt.legend(namelist,loc="upper right")
+plt.legend(namelist,loc="lower right")
 plt.title('Perfil de velocidade em X')
 plt.grid()
+plt.show()
+
+###################################################################
+#Comparando as duas velocidades
+rd = 2
+raio = 40
+
+dy = 1.0
+dx = 1.0
+dt = 1.0
+
+tau = 0.56000000
+
+ni = (2*tau - 1)*(dx*dx) / (6*dt)
+
+v_max = 0.003
+
+dP = v_max * 4 * ni * dx / (raio*raio)
+
+vec = np.zeros([(rd*raio) + 1])
+
+print('ni: ',ni)
+print('dP: ', dP)
+
+for i in range(0,len(vec),1):
+    vec[i] = (dP*(raio*raio - np.abs((raio - i)*(raio - i)))) / (4*ni*dx)
+
+namelist = ['Analitico','Numérico']
+
+plt.plot(vec,'-',c='red')
+plt.plot(vec1,'*',c='blue')
+plt.title('Comparação entre a velocidade analítica e a numérica para 80 pontos - Refinada')
+plt.legend(namelist,loc="upper right")
+plt.grid()
+plt.show()
+
+###############################################################################
+
+fig5 = plt.figure()
+
+erro = np.zeros(len(vec1))
+x    = np.arange(len(vec1))
+
+#não sei se está certinho assim
+for i in range(len(vec1)):
+    erro[i] = np.absolute(vec[i] - vec1[i])
+
+plt.bar(x, erro, align='center', alpha=0.5)
+plt.xticks(x, x)
+plt.ylabel('Diferença')
+plt.xlabel('Posição')
+plt.title('Erro em diferença absoluta')
 plt.show()
